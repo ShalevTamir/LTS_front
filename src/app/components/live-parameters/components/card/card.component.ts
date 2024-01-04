61dcb17dc6acd470839b9728217c7523b668eebe
Shalev Tamir <shalevtamir1009@gmail.com>
2024-01-04 16:06:43 +0200

    empty chart menu working

--- src/app/components/live-parameters/components/card/card.component.ts
+++ src/app/components/live-parameters/components/card/card.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { ChartjsChartComponent } from './chartjs-chart/chartjs-chart.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [ChartjsChartComponent],
+  imports: [ChartjsChartComponent, MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
