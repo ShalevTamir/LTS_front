6459caaa15a96dabaeae62e98dd7043993414109
shalevdev <shalevVarietyDeveloper@gmail.com>
2024-02-03 14:20:03 +0200

    added timepicker to datepicker

--- src/app/components/archive/archive.component.ts
+++ src/app/components/archive/archive.component.ts
@@ -6,17 +6,18 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {provideNativeDateAdapter} from '@angular/material/core';
 import {MatInputModule} from '@angular/material/input';
+import { NgxMatTimepickerModule } from 'ngx-mat-timepicker';
 
 @Component({
   selector: 'app-archive',
   standalone: true,
-  imports: [MatPaginatorModule, MatTableModule, MatFormFieldModule, MatDatepickerModule, MatInputModule],
+  imports: [MatPaginatorModule, MatTableModule, MatFormFieldModule, MatDatepickerModule, MatInputModule, NgxMatTimepickerModule],
   templateUrl: './archive.component.html',
   styleUrl: './archive.component.scss',
   providers: [provideNativeDateAdapter()]
 })
 export class ArchiveComponent implements OnInit{
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('MatPaginator') paginator!: MatPaginator;
   readonly defaultMaxSamples: number = 10;
 
   constructor(private _mongoDBHandler: mongoDBHandlerService){
