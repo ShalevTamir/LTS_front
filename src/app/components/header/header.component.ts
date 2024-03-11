6ad4d4383be031d9b39c9d02efdcf085bb0297b0
Shalev Tamir <shalevtamir1009@gmail.com>
2024-03-11 22:17:43 +0200

    add delete sensors button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,27 +5,34 @@ import { seperateString } from '../../common/utils/string-utils';
 import { SweetAlertsService } from '../../common/services/sweet-alerts.service';
 import { SweetAlertResult } from 'sweetalert2';
 import { SensorHandlerService } from '../../common/services/sensor-handler.service';
-import { NgIf } from '@angular/common';
-import { TELE_PARAMS_ROUTE } from '../../app.routes';
+import { NgIf, NgStyle } from '@angular/common';
+import { DYNAMIC_SENSORS_ROUTE, TELE_PARAMS_ROUTE } from '../../app.routes';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgbModule, NgIf],
+  imports: [NgbModule, NgIf, NgStyle],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   title: string = ""
   liveParamsLoaded: boolean = false;
+  dynamicSensorsPageLoaded: boolean = false;
+  sensorDeletionActive: boolean = false;
   constructor(protected router: Router, private _dynamicSensorService: SensorHandlerService){
     this.router.events.subscribe((event: any) =>{
       if(event instanceof RoutesRecognized){
-        this.liveParamsLoaded = event.url === "/"+TELE_PARAMS_ROUTE;
+        this.liveParamsLoaded = event.url === "/" + TELE_PARAMS_ROUTE;
+        this.dynamicSensorsPageLoaded = event.url === "/" + DYNAMIC_SENSORS_ROUTE;
         let eventUrl: string = event.url;
         this.title = seperateString(eventUrl.substring(1), '-');
       }
     });
+  }
+
+  handleRemoveSensors(){
+    this.sensorDeletionActive = !this.sensorDeletionActive;
   }
 
   async handleAddDynamicSensor(){
