e81d0603fdb853976f24b61a7f12a6689a935f81
Shalev Tamir <shalevtamir1009@gmail.com>
2024-03-12 15:51:19 +0200

    send file to server

--- src/app/components/uploader/uploader.component.ts
+++ src/app/components/uploader/uploader.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { LIVE_DATA_URL, LIVE_TELE_URL } from '../../common/constants';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-uploader',
@@ -8,8 +11,16 @@ import { Component } from '@angular/core';
   styleUrl: './uploader.component.scss'
 })
 export class UploaderComponent {
-  onFileSelected(event: Event){
-    console.log(typeof(event));
+  constructor(private _httpClient: HttpClient){
+
+  }
+  async onFileSelected(event: any){
+    const file: File = event.target.files[0];
+    if(file){
+      const formData = new FormData();
+      formData.append("file", file);
+      console.log(await firstValueFrom(this._httpClient.post(LIVE_TELE_URL+"/live-sensors/sensors-requirements", formData)));
+    }
   }
 
 }
