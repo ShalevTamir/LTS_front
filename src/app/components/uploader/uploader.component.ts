2e3d027d4ec764007bcb2e78ce2370ad5a71dfae
Shalev Tamir <shalevtamir1009@gmail.com>
2024-03-12 13:29:22 +0200

    add styled file upload

--- src/app/components/uploader/uploader.component.ts
+++ src/app/components/uploader/uploader.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './uploader.component.scss'
 })
 export class UploaderComponent {
+  onFileSelected(event: Event){
+    console.log(typeof(event));
+  }
 
 }
