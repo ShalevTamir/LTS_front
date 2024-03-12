2b4a19719944fd54f854423649fd4d1c4d8689af
Shalev Tamir <shalevtamir1009@gmail.com>
2024-03-12 10:49:46 +0200

    add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LiveParametersComponent } from './components/live-parameters/live-parameters.component';
 import { ArchiveComponent } from './components/archive/archive.component';
 import { LiveSensorsComponent } from './components/live-sensors/live-sensors.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const TELE_PARAMS_ROUTE = 'live-telemetry-parameters';
 export const DYNAMIC_SENSORS_ROUTE = 'live-telemetry-sensors';
@@ -9,5 +10,6 @@ export const DYNAMIC_SENSORS_ROUTE = 'live-telemetry-sensors';
 export const routes: Routes = [
     { path: TELE_PARAMS_ROUTE, component: LiveParametersComponent },
     { path: 'archive', component: ArchiveComponent },
-    { path: DYNAMIC_SENSORS_ROUTE, component: LiveSensorsComponent }
+    { path: DYNAMIC_SENSORS_ROUTE, component: LiveSensorsComponent },
+    { path: 'login', component: LoginComponent }
 ];
