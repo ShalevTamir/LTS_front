6a57b0689d68dce3629bac44df66e889fe4bd6cc
Shalev Tamir <shalevtamir1009@gmail.com>
2024-03-27 12:22:06 +0200

    implement gaurds

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ArchiveComponent } from './components/archive/archive.component';
 import { LiveSensorsComponent } from './components/live-sensors/live-sensors.component';
 import { UploaderComponent } from './components/requirements-uploader/requirements-uploader.component';
 import { AuthComponent } from './components/auth/auth.component';
+import { tokenGaurd } from './common/gaurds/TokenGaurd';
 
 export const TELE_PARAMS_ROUTE = 'live-telemetry-parameters';
 export const HOME_ROUTE = TELE_PARAMS_ROUTE;
@@ -12,10 +13,10 @@ export const LOGIN_ROUTE = 'login';
 export const SIGNUP_ROUTE = 'signup';
 
 export const routes: Routes = [
-    { path: TELE_PARAMS_ROUTE, component: LiveParametersComponent },
-    { path: 'archive', component: ArchiveComponent },
-    { path: DYNAMIC_SENSORS_ROUTE, component: LiveSensorsComponent },
+    { path: TELE_PARAMS_ROUTE, component: LiveParametersComponent, canActivate: [tokenGaurd] },
+    { path: 'archive', component: ArchiveComponent, canActivate: [tokenGaurd] },
+    { path: DYNAMIC_SENSORS_ROUTE, component: LiveSensorsComponent, canActivate: [tokenGaurd] },
     { path: LOGIN_ROUTE, component: AuthComponent },
     { path: SIGNUP_ROUTE, component: AuthComponent },
-    { path: 'upload-sensors-requirements', component: UploaderComponent}
+    { path: 'upload-sensors-requirements', component: UploaderComponent, canActivate: [tokenGaurd]}
 ];
---
+++ src/app/common/gaurds/TokenGaurd.ts
@@ -0,0 +1,19 @@
+import { CanActivateFn, Router } from "@angular/router";
+import { TOKEN_STORAGE_KEY } from "../models/constants";
+import { isNullOrUndef } from "../utils/helper";
+import { SweetAlertsService } from "../services/sweet-alerts.service";
+import { inject } from "@angular/core";
+import { LOGIN_ROUTE } from "../../app.routes";
+
+export const tokenGaurd: CanActivateFn = () => {
+    const swalService = inject(SweetAlertsService);
+    const router = inject(Router);
+    if (isNullOrUndef(localStorage.getItem(TOKEN_STORAGE_KEY))){
+        swalService.errorAlert("You must login before accessing this page");
+        router.navigateByUrl(LOGIN_ROUTE);
+        return false;
+    }
+    else{
+        return true;
+    }
+}
